b9a12aa2f7e2ce1d6a83daac9c8f1aa4dfc05ff2
hansungj <43443895+hansungj@users.noreply.github.com>
2020-04-14 22:50:13 -0400

    Update MVO.m

--- MVO.m
+++ MVO.m
@@ -1,16 +1,8 @@
 function  x_optimal = MVO(mu, Q, targetRet)
     
-    % Use this function to construct your MVO portfolio subject to the
-    % target return, with short-selling allowed. 
-    %
-    % You may use quadprog, Gurobi, or any other optimizer you are familiar
-    % with. Just be sure to comment on your code to (briefly) explain your
-    % procedure.
-
     % Find the total number of assets
     n = size(Q,1); 
-    % *************** WRITE YOUR CODE HERE ***************
-    %----------------------------------------------------------------------
+
     %budget constraint
     Aeq = ones(1,n); 
     % A = -mu
@@ -21,11 +13,5 @@ function  x_optimal = MVO(mu, Q, targetRet)
     
     
     
-    
-    
-    
-    
-
-    %----------------------------------------------------------------------
     
 end
